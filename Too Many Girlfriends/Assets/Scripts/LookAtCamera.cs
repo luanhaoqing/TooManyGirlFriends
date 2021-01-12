dab16df861f5fc07f4a1d1c7767546536cf08547
luanhaoqing <luanhaoqing@hotmail.com>
2021-01-11 22:31:05 -0800

    Make bubble always face to camera.

---
+++ Too Many Girlfriends/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private GameObject camera;
+    private Vector3 cameraFacing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        cameraFacing = camera.transform.forward;
+        this.transform.forward = -cameraFacing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.forward = -cameraFacing;
+    }
+}
